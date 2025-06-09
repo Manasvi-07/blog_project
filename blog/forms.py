05dd0fa75ea36aebe270839a777feb2188fd64b6
Mac <mac@Taglines-iMac-Pro-106.local>
2025-06-09 09:22:02 +0530

    upadate files

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,8 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm
 from .models import Post
 
-class RegisterForm(AuthenticationForm):
+class RegisterForm(forms.ModelForm):
     password = forms.CharField(label='Password',widget=forms.PasswordInput)
     password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
 
@@ -12,14 +11,20 @@ class RegisterForm(AuthenticationForm):
         fields = ['username', 'email']
 
     def clean(self):
-        cleaned_data = super().clean
+        cleaned_data = super().clean()
         password = cleaned_data.get("password")
         password2 = cleaned_data.get("password2")
 
         if password and password2 and password != password2:
             raise forms.ValidationError("password not match")
         return cleaned_data
-
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password']) 
+        if commit:
+            user.save()
+        return user
 
 class PostForm(forms.ModelForm):
     class Meta:

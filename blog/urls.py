68807443502858b6c7c2121a754ae3038a8620b2
Mac <mac@Taglines-iMac-Pro-106.local>
2025-06-13 09:58:54 +0530

    Update files

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+
 from .views import PostList, PostCreateView, PostUpdateView, PostDeleteView, PostDetailView, HomeView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('accounts/', include('accounts.urls', namespace='accounts')),
     path('posts/', PostList.as_view(), name='post_list'),
     path('posts/create/', PostCreateView.as_view(), name='create_post'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
@@ -16,7 +14,6 @@ urlpatterns = [
 ]
 
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
     
